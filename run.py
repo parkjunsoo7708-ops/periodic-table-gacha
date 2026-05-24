5edbc7981f726731b7a48a87d1120258c565d50f
parkjunsoo7708-ops <parkjunsoo7708@gmail.com>
2026-05-24 22:58:29 +0900

    run.py 업데이트

--- run.py
+++ run.py
@@ -286,3 +286,286 @@ for item in draw_pool:
 current_weight += item['weight']
 if random_val <= current_weight:
 	return item['Ag']
+
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Cd']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['In']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Sn']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Sb']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Te']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['I']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Xe']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Cs']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Ba']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['La']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Ce']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Pr']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Nd']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Pm']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Sm']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Eu']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Gd']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Tb']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Dy']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Ho']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Er']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Tm']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Yb']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Lu']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Hf']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Ta']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['W']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Re']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Os']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Ir']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Pt']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Au']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Hg']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Tl']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Pb']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Bi']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Po']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['At']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Rn']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Fr']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Ra']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Ac']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Th']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Pa']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['U']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Np']
+
+current_weight = 0
+for item in draw_pool:
+current_weight += item['weight']
+if random_val <= current_weight:
+	return item['Pu']
