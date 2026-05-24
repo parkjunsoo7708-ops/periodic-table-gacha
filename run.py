dc64114fa507e1409905aa303f6b4b468d23584a
parkjunsoo7708-ops <parkjunsoo7708@gmail.com>
2026-05-24 23:20:47 +0900

    run.py 업데이트

--- run.py
+++ run.py
@@ -1,576 +1,170 @@
 import random
 
-def gacha(draw_pool)
-total_weight = 94
-
-random_val = random.uniform(0, 94)
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['H']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['He']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Li']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Be']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['B']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['C']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['N']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['O']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['F']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ne']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Na']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Mg']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Al']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Si']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['P']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['S']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Cl']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ar']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['K']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ca']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Sc']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ti']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['V']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Cr']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Mn']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Fe']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Co']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ni']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Cu']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Zn']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ga']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ge']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['As']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Se']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Br']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Kr']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Rb']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Sr']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Y']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Zr']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Nb']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Mo']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Tc']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ru']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Rh']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Pd']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ag']
-
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Cd']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['In']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Sn']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Sb']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Te']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['I']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Xe']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Cs']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ba']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['La']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ce']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Pr']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Nd']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Pm']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Sm']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Eu']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Gd']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Tb']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Dy']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ho']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Er']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Tm']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Yb']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Lu']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Hf']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ta']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['W']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Re']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Os']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ir']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Pt']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Au']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Hg']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Tl']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Pb']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Bi']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Po']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['At']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Rn']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Fr']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ra']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Ac']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Th']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Pa']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['U']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Np']
-
-current_weight = 0
-for item in draw_pool:
-current_weight += item['weight']
-if random_val <= current_weight:
-	return item['Pu']
-	
-	items = ['SSR', 'SR', 'R']
-	probabilities[3, 17, 80]
-	
-	print(f"congrats! {draw_gacha()} you got rarity)
+# ── Rarity tiers ──────────────────────────────────────────────────────────────
+# SSR  3%  – noble gases, precious/platinum-group metals, radioactive elements
+# SR  17%  – transition metals, metalloids, halogens
+# R   80%  – nonmetals, alkali/alkaline-earth metals, common post-transition metals
+
+ELEMENTS = [
+    # symbol, name, atomic number, rarity
+    ("H",  "Hydrogen",      1,  "R"),
+    ("He", "Helium",        2,  "SSR"),
+    ("Li", "Lithium",       3,  "R"),
+    ("Be", "Beryllium",     4,  "SR"),
+    ("B",  "Boron",         5,  "SR"),
+    ("C",  "Carbon",        6,  "R"),
+    ("N",  "Nitrogen",      7,  "R"),
+    ("O",  "Oxygen",        8,  "R"),
+    ("F",  "Fluorine",      9,  "SR"),
+    ("Ne", "Neon",         10,  "SSR"),
+    ("Na", "Sodium",       11,  "R"),
+    ("Mg", "Magnesium",    12,  "R"),
+    ("Al", "Aluminum",     13,  "R"),
+    ("Si", "Silicon",      14,  "R"),
+    ("P",  "Phosphorus",   15,  "R"),
+    ("S",  "Sulfur",       16,  "R"),
+    ("Cl", "Chlorine",     17,  "SR"),
+    ("Ar", "Argon",        18,  "SSR"),
+    ("K",  "Potassium",    19,  "R"),
+    ("Ca", "Calcium",      20,  "R"),
+    ("Sc", "Scandium",     21,  "SR"),
+    ("Ti", "Titanium",     22,  "SR"),
+    ("V",  "Vanadium",     23,  "SR"),
+    ("Cr", "Chromium",     24,  "SR"),
+    ("Mn", "Manganese",    25,  "SR"),
+    ("Fe", "Iron",         26,  "R"),
+    ("Co", "Cobalt",       27,  "SR"),
+    ("Ni", "Nickel",       28,  "SR"),
+    ("Cu", "Copper",       29,  "SR"),
+    ("Zn", "Zinc",         30,  "R"),
+    ("Ga", "Gallium",      31,  "SR"),
+    ("Ge", "Germanium",    32,  "SR"),
+    ("As", "Arsenic",      33,  "SR"),
+    ("Se", "Selenium",     34,  "SR"),
+    ("Br", "Bromine",      35,  "SR"),
+    ("Kr", "Krypton",      36,  "SSR"),
+    ("Rb", "Rubidium",     37,  "SR"),
+    ("Sr", "Strontium",    38,  "SR"),
+    ("Y",  "Yttrium",      39,  "SR"),
+    ("Zr", "Zirconium",    40,  "SR"),
+    ("Nb", "Niobium",      41,  "SR"),
+    ("Mo", "Molybdenum",   42,  "SR"),
+    ("Tc", "Technetium",   43,  "SSR"),
+    ("Ru", "Ruthenium",    44,  "SSR"),
+    ("Rh", "Rhodium",      45,  "SSR"),
+    ("Pd", "Palladium",    46,  "SSR"),
+    ("Ag", "Silver",       47,  "SSR"),
+    ("Cd", "Cadmium",      48,  "SR"),
+    ("In", "Indium",       49,  "SR"),
+    ("Sn", "Tin",          50,  "R"),
+    ("Sb", "Antimony",     51,  "SR"),
+    ("Te", "Tellurium",    52,  "SR"),
+    ("I",  "Iodine",       53,  "SR"),
+    ("Xe", "Xenon",        54,  "SSR"),
+    ("Cs", "Cesium",       55,  "SR"),
+    ("Ba", "Barium",       56,  "SR"),
+    ("La", "Lanthanum",    57,  "SR"),
+    ("Ce", "Cerium",       58,  "SR"),
+    ("Pr", "Praseodymium", 59,  "SR"),
+    ("Nd", "Neodymium",    60,  "SR"),
+    ("Pm", "Promethium",   61,  "SSR"),
+    ("Sm", "Samarium",     62,  "SR"),
+    ("Eu", "Europium",     63,  "SR"),
+    ("Gd", "Gadolinium",   64,  "SR"),
+    ("Tb", "Terbium",      65,  "SR"),
+    ("Dy", "Dysprosium",   66,  "SR"),
+    ("Ho", "Holmium",      67,  "SR"),
+    ("Er", "Erbium",       68,  "SR"),
+    ("Tm", "Thulium",      69,  "SR"),
+    ("Yb", "Ytterbium",    70,  "SR"),
+    ("Lu", "Lutetium",     71,  "SR"),
+    ("Hf", "Hafnium",      72,  "SR"),
+    ("Ta", "Tantalum",     73,  "SR"),
+    ("W",  "Tungsten",     74,  "SR"),
+    ("Re", "Rhenium",      75,  "SR"),
+    ("Os", "Osmium",       76,  "SSR"),
+    ("Ir", "Iridium",      77,  "SSR"),
+    ("Pt", "Platinum",     78,  "SSR"),
+    ("Au", "Gold",         79,  "SSR"),
+    ("Hg", "Mercury",      80,  "SR"),
+    ("Tl", "Thallium",     81,  "SR"),
+    ("Pb", "Lead",         82,  "R"),
+    ("Bi", "Bismuth",      83,  "SR"),
+    ("Po", "Polonium",     84,  "SSR"),
+    ("At", "Astatine",     85,  "SSR"),
+    ("Rn", "Radon",        86,  "SSR"),
+    ("Fr", "Francium",     87,  "SSR"),
+    ("Ra", "Radium",       88,  "SSR"),
+    ("Ac", "Actinium",     89,  "SSR"),
+    ("Th", "Thorium",      90,  "SSR"),
+    ("Pa", "Protactinium", 91,  "SSR"),
+    ("U",  "Uranium",      92,  "SSR"),
+    ("Np", "Neptunium",    93,  "SSR"),
+    ("Pu", "Plutonium",    94,  "SSR"),
+]
+
+RARITY_WEIGHTS = {
+    "SSR": 3,
+    "SR":  17,
+    "R":   80,
+}
+
+
+def gacha(draw_pool=None):
+    """
+    Perform a gacha draw and return one element.
+
+    Args:
+        draw_pool: Optional list of dicts, each with keys:
+                       'symbol', 'name', 'atomic_number', 'rarity'
+                   Defaults to all 94 elements defined above.
+
+    Returns:
+        dict with keys: symbol, name, atomic_number, rarity
+    """
+    if draw_pool is None:
+        draw_pool = [
+            {
+                "symbol":        sym,
+                "name":          name,
+                "atomic_number": num,
+                "rarity":        rarity,
+            }
+            for sym, name, num, rarity in ELEMENTS
+        ]
+
+    if not draw_pool:
+        raise ValueError("draw_pool cannot be empty")
+
+    # Step 1 – pick a rarity tier based on overall probabilities
+    rarity_roll = random.random()  # [0.0, 1.0)
+
+    if rarity_roll < 0.03:
+        chosen_rarity = "SSR"
+    elif rarity_roll < 0.20:   # 0.03 + 0.17
+        chosen_rarity = "SR"
+    else:
+        chosen_rarity = "R"
+
+    # Step 2 – filter pool to that tier; fall back to full pool if tier is empty
+    tier_pool = [item for item in draw_pool if item.get("rarity") == chosen_rarity]
+    if not tier_pool:
+        tier_pool = draw_pool
+
+    # Step 3 – uniform draw within the tier (each element equally likely)
+    return random.choice(tier_pool)
+
+
+# ── Example usage ──────────────────────────────────────────────────────────────
+if __name__ == "__main__":
+    items = ["SSR", "SR", "R"]
+    probabilities = [3, 17, 80]   # percent chance per tier
+
+    print("Single draw:")
+    result = gacha()
+    print(f"  [{result['rarity']}] {result['symbol']} – {result['name']} (Z={result['atomic_number']})")
+
+    print("\n10-pull:")
+    for _ in range(10):
+        r = gacha()
+        print(f"  [{r['rarity']}] {r['symbol']:2s} – {r['name']}")
